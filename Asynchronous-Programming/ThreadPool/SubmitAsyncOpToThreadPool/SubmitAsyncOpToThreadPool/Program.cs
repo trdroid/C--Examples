864a2d9db3cbf4c599e0aa8ea288c4051c5a2185
Jai Srikar <gruprog@gmail.com>
2017-07-15 16:21:50 -0700

    sample example using ThreadPool

---
+++ Asynchronous-Programming/ThreadPool/SubmitAsyncOpToThreadPool/SubmitAsyncOpToThreadPool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace SubmitAsyncOpToThreadPool {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Start of the Main thread {0}", Thread.CurrentThread.ManagedThreadId);
+            ThreadPool.QueueUserWorkItem(AsyncOperation);
+            Thread.Sleep(TimeSpan.FromSeconds(15));
+            Console.WriteLine("End of the Main thread {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.ReadKey();        
+        }
+
+        private static void AsyncOperation(object operationId) {
+            Console.WriteLine($"Performing Operation {(operationId == null? "N/A" : operationId)} on Worker Thread {Thread.CurrentThread.ManagedThreadId} of the ThreadPool");
+            Thread.Sleep(TimeSpan.FromSeconds(8));
+            Console.WriteLine($"End of Async Operation {(operationId == null ? "N/A" : operationId)} on Worker Thread {Thread.CurrentThread.ManagedThreadId} of the ThreadPool");
+        }
+    }
+}
